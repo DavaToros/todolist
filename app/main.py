c60efc4ecf3399d262b7bfee4758dcf372ddaaec
David Torosyan <91628430+DavaToros@users.noreply.github.com>
2026-06-09 21:20:11 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.database import init_db
 from app.routes import router
 
-app = FastAPI(title="Todo API", description="Простой REST API для задач", version="1.0")
-
-@app.on_event("startup")
-def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     init_db()
+    yield
+
+app = FastAPI(title="todo api", description="todo лист", lifespan=lifespan)
 
 app.include_router(router)
